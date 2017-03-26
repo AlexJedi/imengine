56b55bbf398902c2bd09f95560c4f23b9fb0f690
Liza Kobrazova <lizakobrazova@gmail.com>
2017-03-26 08:41:16 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template lang='pug'>
+  .app
+    about
+</template>
+
+<script>
+import About from '@/components/About';
+
+export default {
+  name: 'app',
+  components: {
+    About,
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/About.vue
@@ -0,0 +1,13 @@
+<template lang='pug'>
+  .about
+    h1 HERE is JOHNNY
+</template>
+
+<script>
+export default {
+  name: 'about',
+};
+</script>
+
+<style lang='scss'>
+</style>
