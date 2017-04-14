fdf438c77ae75506b01e1a7c9e7da158eec589a9
Liza Kobrazova <lizakobrazova@gmail.com>
2017-04-14 13:02:37 +0300

    Add Gallery page

---
+++ src/components/Gallery.vue
@@ -0,0 +1,16 @@
+<template lang='pug'>
+  .gallery
+</template>
+
+<script>
+export default {
+  name: 'gallery',
+  components: {},
+};
+</script>
+
+<style lang='scss'>
+.gallery {
+  width: 100%;
+}
+</style>
